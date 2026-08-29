cc6191fcac6094737125b86c1a672862e7ce6d9c
Raghavi M <raghavikanchi@gmail.com>
2026-08-29 09:42:55 +0530

    Add quiz questions and scoring

--- src/Main.java
+++ src/Main.java
@@ -16,9 +16,85 @@ System.out.print("Enter your name: ");
 
 String name = scanner.nextLine();
 
-System.out.println("\nWelcome, name!");
+Question[] questions = {
 
-System.out.println("Lets start the quiz.");
+Question(
+
+"Which language is mainly used for Android development?"
+
+new String[]{"Python" "Java" "C" "HTML"}
+
+2
+
+)
+
+new Question(
+
+"Which keyword is used to create a class in Java?"
+
+new String[]{"class" "define" "struct" "new"}
+
+1
+
+)
+
+new Question(
+
+"Which method is the starting point of a Java program?"
+
+new String[]{"start()" "run()" "main()" "execute()"}
+
+3
+
+)
+
+};
+
+int score = 0;
+
+for (int i = 0; i < questions.length; i++) {
+
+System.out.println("\nQuestion " + (i + 1) + ":");
+
+System.out.println(questions[i].question);
+
+for (int j = 0; j < questions[i].options.length;, j++) {
+
+System.out.println((j + 1) + ". " + Questions[i].options[j]);
+
+}
+
+System.out.print("Enter your answer: ");
+
+int answer = scanner.nextInt();
+
+if (answer == questions[i].correctAnswer) {
+
+System.out.println("Correct!");
+
+score++;
+
+}
+
+System.out.println("Wrong!");
+
+}
+
+}
+
+System.out.println("\n================================");
+
+System.out.println("             RESULT");
+
+System.out.println("================================");
+
+System.out.println("Student: " + name);
+
+System.out.println("Score: " + score + "/" + questions.length);
+
+double percentage = (score * 100.0) / questions.length;
+
+System.out.println("Percentage: " + percentage + "%");
 
 scanner.close();
 
