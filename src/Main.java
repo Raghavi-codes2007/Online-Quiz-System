4c70a4aab36bf273b4aa347b84a7d68dd12903d5
Raghavi M <raghavikanchi@gmail.com>
2026-08-29 09:34:58 +0530

    Create Main Java file

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+class Main {
+
+public static void main(String[] args) {
+
+Scanner scanner = new Scanner(System.in);
+
+System.out.println("================================");
+
+System.out.println("       ONLINE QUIZ SYSTEM");
+
+System.out.println("================================");
+
+System.out.print("Enter your name: ");
+
+String name = scanner.nextLine();
+
+System.out.println("\nWelcome, name!");
+
+System.out.println("Lets start the quiz.");
+
+scanner.close();
+
+}
+
+}
