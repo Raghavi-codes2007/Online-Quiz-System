56f8149d20d5ed518f84de7f7349856b2efa6ad3
Raghavi M <raghavikanchi@gmail.com>
2026-08-29 09:37:24 +0530

    Create Question.java

---
+++ src/Question.java
@@ -0,0 +1,19 @@
+public class Question {
+
+String question;
+
+String[] options;
+
+int correctAnswer;
+
+Question(String question, String[] options, int correctAnswer) {
+
+this.question = question;
+
+this.options = options;
+
+this.correctAnswer = correctAnswer;
+
+}
+
+}
